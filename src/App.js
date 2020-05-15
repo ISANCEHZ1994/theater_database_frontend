6d676771229c66db0493a16160f069c8db45d089
Israel <Israel.pro1994@gmail.com>
2020-05-15 16:29:56 -0500

    adding a Hook for Search functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Search from './Components/Search';
 
 export default function App() {
+
+  const [state, setState] = ({
+    s:"", // a string to search the movie you are looking for
+    results: [], // where we want to put our search results into
+    selected: {} // when selecting a SPECIFIC movie - we want to see a everything that has to do with that movie
+  });
+
+  const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=91302107';
+
+  handleInput = (e) => { 
+    let s = e.target.value
+
+    setState(preState => { // setState is a function special to React that changes the state of a given variable - 
+      return { ...preState, s: s} 
+    })
+  };
+
   return (
     <div className="App">
         <header>
