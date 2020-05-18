30f6c912902a3f8d7c653c0cac510028de4b9939
Israel <Israel.pro1994@gmail.com>
2020-05-18 18:14:57 -0500

    installed axios for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import Search from './Components/Search';
+import axios from 'axios';
 
 export default function App() {
 
-  const [state, setState] = ({
+  const [state, setState] = useState({ // we are using hooks and giving 
     s:"", // a string to search the movie you are looking for
     results: [], // where we want to put our search results into
     selected: {} // when selecting a SPECIFIC movie - we want to see a everything that has to do with that movie
@@ -11,21 +12,38 @@ export default function App() {
 
   const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=91302107';
 
-  handleInput = (e) => { 
+  const search = (e) => {
+    if(e.key === 'Enter'){ // make sure the e in Enter is CAPITALIZE 
+      axios(apiUrl + "&s=" + state.s).then( ({ data }) => { // we want to destructure the data variable because we only want to use some specifc data
+        let results = data.Search
+
+        setState( prevState => {
+          return {...prevState, results: results} // the search function will take the information from the axios(technically a fetch) and store it into the array 'results'
+        })
+        // console.log(data)
+      }) // closes then
+    }; // closes if
+  }; // closes function
+
+  const handleInput = (e) => { 
     let s = e.target.value
 
-    setState(preState => { // setState is a function special to React that changes the state of a given variable - 
+    setState(preState => { // setState is the method that we made in the hooks above because we - since App function is not a class - a this. is not required
       return { ...preState, s: s} 
     })
   };
 
+  console.log(state.s)
   return (
     <div className="App">
         <header>
           <h1> Movie DataBase </h1>
         </header>
         <main>
-          <Search />
+          <Search 
+          handleInput={handleInput}
+          search={search}
+          />
         </main>
     </div>
   );
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
 
-// export default class Search extends React.Component{
-
-//     render(){
-//         return(
-//             <>
-//             </>
-//         )
-//     };
-
-// };
-
-export default function Search(){
+export default function Search ({handleInput,search}) { // destructuring assignment - we're diving deeper into the props program where we dont need to pass props
         return(
           <section className='searchbox-wrap'>
-              <input type='text' placeholder='Search For A Movie...' className='searchbox'/>
+              <input 
+                    type='text' 
+                    placeholder='Search For A Movie...' 
+                    className='searchbox' 
+                    onChange={handleInput} 
+                    onKeyPress={search}
+              /> {/* this makes it look cleaner */}
           </section>
         )
 };
